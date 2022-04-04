7386254c0f9bb9bb225f249803e19984e61dd018
manon.dauchy <dauchy.manon@hotmail.com>
2022-04-04 11:44:36 +0200

    accueil1

---
+++ app/src/main/java/fr/isen/dauchy/applogkey/AccueilActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.dauchy.applogkey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccueilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil)
+    }
+}
