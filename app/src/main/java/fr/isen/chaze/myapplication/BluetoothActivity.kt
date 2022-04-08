aaf649d097b54cb9d60779642f85e29562940e82
MarieChaze <82362170+MarieChaze@users.noreply.github.com>
2022-04-08 16:17:04 +0200

    up 2

---
+++ app/src/main/java/fr/isen/chaze/myapplication/BluetoothActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.chaze.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+}
---
+++ app/src/main/java/fr/isen/chaze/myapplication/CheckInActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.chaze.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_in)
+    }
+}
