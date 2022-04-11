9e19615eb55051d75acf0587bd9acd7ba81d9d45
0Davgi0 <davgi.wifeo@gmail.com>
2022-04-11 11:10:07 +0200

    Erase

--- app/src/main/java/fr/isen/dauchy/applogkey/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package fr.isen.dauchy.applogkey.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
