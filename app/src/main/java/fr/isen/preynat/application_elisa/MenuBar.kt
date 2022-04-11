5ef150871fc7e289486feca89d3285a55c919100
elisa-preynat <81637789+elisa-preynat@users.noreply.github.com>
2022-04-11 14:10:30 +0200

    up3

---
+++ app/src/main/java/fr/isen/preynat/application_elisa/MenuBar.kt
@@ -0,0 +1,11 @@
+package fr.isen.preynat.application_elisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_bar)
+    }
+}
