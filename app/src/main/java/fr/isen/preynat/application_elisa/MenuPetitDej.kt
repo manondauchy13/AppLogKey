7f8344c391397dfd0cac9ce0d7d281345585f6f7
0Davgi0 <davgi.wifeo@gmail.com>
2022-04-11 14:16:44 +0200

    Ajout du travail déjà effectué

---
+++ app/src/main/java/fr/isen/preynat/application_elisa/MenuPetitDej.kt
@@ -0,0 +1,11 @@
+package fr.isen.preynat.application_elisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuPetitDej : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_petit_dej)
+    }
+}
---
+++ app/src/main/java/fr/isen/preynat/application_elisa/MonCompte.kt
@@ -0,0 +1,11 @@
+package fr.isen.preynat.application_elisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MonCompte : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mon_compte)
+    }
+}
